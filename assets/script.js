82347c08379f266f64d20433497983db6fad0822
coc lay <coc.lay97@gmail.com>
2023-04-03 22:15:38 -0400

    add fn for formhandler

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 var cityListEl = document.querySelector("#city-list");
 var cityFormEl = document.querySelector("#city-search");
 var cityInputEl = document.querySelector("#city-input");
+var cityContainerEl = document.querySelector("#city-container")
+var theCity = document.querySelector("#city-name")
+var temp = document.querySelector("#temp")
+var wing = document.querySelector("#wind")
+var humidity = document.querySelector("#humidity")
 var cities = [];
 
 function renderCity() {
@@ -39,4 +44,38 @@ function storeCity() {
     cityInputEl.value = "";
     storeCity();
     renderCity();
- })
+ });
+
+ function cityFormHandler(event) {
+    event.preventDefault();
+    var cityName = cityInputEl.value.trim();
+    if(cityName) {
+        getCity(cityName);
+        cityContainerEl.textContent = "";
+        cityInputEl.value = "";
+    }else {
+        alert("Please enter a city!");
+    }
+ }
+
+ function getCity(city) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=33.44&lon=-94.04&appid=636fa7103c23a27dc3dbfcfae373ccaf'
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            console.log(response)
+            response.json().then(function(data) {
+                console.log(data)
+                displayWeather(data, city);
+            })
+        }
+    })
+ }
+
+ function displayWeather(cityName, searchedCity) {
+    if(cityName === 0) {
+        return;
+    }
+    theCity.textContent = searchedCity;
+    temp.textContent = 
+ }
